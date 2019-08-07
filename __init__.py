1bbd6ef25344ca92280bfa620d5e872837620b9c
fissanpovlsen <rasmus_bodker@hotmail.com>
2019-08-07 15:25:57 +0200

    Added __init__.py file

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 10:04:12 2019
+
+@author: rbm
+"""
+
+#__all__ = ['gauss_chol','precal_cov']
+from .gauss_chol import gauss_chol
+from .precal_cov import precal_cov
