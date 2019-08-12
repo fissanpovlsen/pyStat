7c4c0ff4feee196724bdb640f8a4b63eff1afbd6
fissanpovlsen <rasmus_bodker@hotmail.com>
2019-08-12 13:46:57 +0200

    Worked on sgs

--- sgs.py
+++ sgs.py
@@ -26,7 +26,10 @@ The second point means that our data set for kriging increases by one data point
 import numpy as np
 import matplotlib.pyplot as plt
 import rasmus_py_funcs as rfuncs
+import scipy.stats as stats
 import scipy as sp
+import pykrige.kriging_tools as kt
+from pykrige.ok import OrdinaryKriging
 
 #import time
 #import scipy.stats
@@ -38,28 +41,43 @@ import scipy as sp
 # 
 #import random
 #
-#def cdf( d, bins=12 ):
-#    import numpy as np
-#    """
-#    Function creates a cumulative distribution function, and its inverse, for a given data set d. 
-#    The CDF and its inverse are reported as <Nx2> arrays, where N is the number of data points in d, the input data set.
-#    """
-#    N = len( d )
-#    counts, intervals = np.histogram( d, bins=bins )
-#    h = np.diff( intervals ) / 2.0
-#    f, finv = np.zeros((N,2)), np.zeros((N,2))
-#    idx, k, T = 0, 0, float( np.sum( counts ) )
-#    for count in counts:
-#        for i in range( count ):
-#            x = intervals[idx]+h[0]
-#            y = np.cumsum( counts[:idx+1] )[-1] / T
-#            f[k,:] = x, y 
-#            finv[k,:] = d.max()-x,1-y
-#            k += 1
-#        idx += 1
-#    return f, finv
+def SVh( P, h, bw ):
+    from scipy.spatial.distance import pdist, squareform
+    '''
+    Experimental semivariogram for a single lag
+    '''
+    pd = squareform( pdist( P[:,:2] ) )
+    N = pd.shape[0]
+    Z = list()
+    for i in range(N):
+        for j in range(i+1,N):
+            if( pd[i,j] >= h-bw )and( pd[i,j] <= h+bw ):
+                Z.append( ( P[i,2] - P[j,2] )**2.0 )
+    return np.sum( Z ) / ( 2.0 * len( Z ) )
+ 
+def SV( P, hs, bw ):
+    '''
+    Experimental variogram for a collection of lags
+    '''
+    sv = list()
+    for h in hs:
+        sv.append( SVh( P, h, bw ) )
+    sv = [ [ hs[i], sv[i] ] for i in range( len( hs ) ) if sv[i] > 0 ]
+    return np.array( sv ).T
+ 
+def C( P, h, bw ):
+    '''
+    Calculate the sill
+    '''
+    c0 = np.var( P[:,2] )
+    if h == 0:
+        return c0
+    return c0 - SVh( P, h, bw )
 
-def cdf2(d,num_bins=20):
+
+
+
+def calculate_cdf(d,num_bins=20):
     import numpy as np
     counts, bin_edges = np.histogram (d, bins=num_bins)
     cdf = np.cumsum (counts)
@@ -70,38 +88,71 @@ def cdf2(d,num_bins=20):
     #cdf[:,0] = np.cumsum(d)/np.sum(d)
     #cdf[:,1] = 1-cdf[:,0]    
     return cdf,bin_edges
-#
-#def fit( d ):
-#    x, y = d[:,0], d[:,1]
-#    def f(t):
-#        if t <= x.min():
-#            return y[ np.argmin(x) ]
-#        elif t >= x.max():
-#            return y[ np.argmax(x) ]
-#        else:
-#            intr = sp.interpolate.interp1d( x, y )
-#            return intr(t)
-#    return f
-#
-#
-## transform data to normal dist
-#def to_norm( data, bins=12 ):
-#    mu = np.mean( data )
-#    sd = np.std( data )
-#    z = ( data - mu ) / sd
-#    f, inv = cdf( z, bins=bins )
-#    z = sp.stats.norm(0,1).ppf( f[:,1] )
-#    z = np.where( z==np.inf, np.nan, z )
-#    z = np.where( np.isnan( z ), np.nanmax( z ), z )
-#    param = ( mu, sd )
-#    return z, inv, param, mu, sd
-# 
-## transform data from normal dist back
-#def from_norm( data, inv, param, mu, sd ):
-#    h = fit( inv )
-#    f = sp.stats.norm(0,1).cdf( data )
-#    z = [ h(i)*sd + mu for i in f ]
-#    return z
+
+def get_index_for_data_on_grid(data_sparse,data):
+    k = 0
+    duplicate = np.zeros([50,1])# Duplicate index
+    for i in range(Nd):
+        x_test = np.where((data_sparse[:,0]==data[i,0]))
+        y_test = np.where((data_sparse[:,1]==data[i,1]))
+        if (len(x_test[0])>0 and len(y_test[0])>0):
+            for ii in range(len(x_test[0])):
+                for iii in range(len(y_test[0])):
+                    if (x_test[0][ii]==y_test[0][iii]).any():
+                        duplicate[k] = i
+                        k += 1
+                        print(k)
+    non_duplicate = np.delete(np.arange(500),[duplicate]) # Non-duplicate index
+    return non_duplicate,duplicate
+    
+def opt( fct, x, y, C0, parameterRange=None, meshSize=1000 ):
+    if parameterRange == None:
+        parameterRange = [ x[1], x[-1] ]
+    mse = np.zeros( meshSize )
+    a = np.linspace( parameterRange[0], parameterRange[1], meshSize )
+    for i in range( meshSize ):
+        mse[i] = np.mean( ( y - fct( x, a[i], C0 ) )**2.0 )
+    return a[ mse.argmin() ]
+
+def spherical( h, a, C0 ):
+    '''
+    Spherical model of the semivariogram
+    '''
+    # if h is a single digit
+    if type(h) == np.float64:
+        # calculate the spherical function
+        if h <= a:
+            return C0*( 1.5*h/a - 0.5*(h/a)**3.0 )
+        else:
+            return C0
+    # if h is an iterable
+    else:
+        # calcualte the spherical function for all elements
+        a = np.ones( h.size ) * a
+        C0 = np.ones( h.size ) * C0
+        return map( spherical, h, a, C0 )
+
+
+def cvmodel( P, model, hs, bw ):
+    '''
+    Input:  (P)      ndarray, data
+            (model)  modeling function
+                      - spherical
+                      - exponential
+                      - gaussian
+            (hs)     distances
+            (bw)     bandwidth
+    Output: (covfct) function modeling the covariance
+    '''
+    # calculate the semivariogram
+    sv = SV( P, hs, bw )
+    # calculate the sill
+    C0 = C( P, hs[0], bw )
+    # calculate the optimal parameters
+    param = opt( model, sv[0], sv[1], C0 )
+    # return a covariance function
+    covfct = lambda h, a=param: C0 - model( h, a, C0 )
+    return covfct
 
 # Create some data for testing
 Nd = 500    
@@ -139,32 +190,35 @@ data_sparse[:,3] = sp.stats.zscore(data_sparse[:,2]) # Calculate z-score
 
 # 3. Visit the rest of the grid cells at random and perform kriging using all of the data available
 # Define random path
-k = 0
-remind = np.zeros([50,1])# Remove index
-new_dat = np.copy(data)  
-for i in range(Nd):
-    x_test = np.where((data_sparse[:,0]==data[i,0]))
-    y_test = np.where((data_sparse[:,1]==data[i,1]))
-    print(i)
-    if (x_test[0][0]==y_test[0][0]).all():
-        remind[k] = i
-        k += 1
-    
+non_duplicate,duplicate = get_index_for_data_on_grid(data_sparse,data) # Getting indexes where there is no data
+randpath = np.random.permutation(non_duplicate) # randomize this array
+
+
 
 # Calculate cdfs
 num_bins = 10
-cdf_data,bin_edges = cdf2(data[:,2],num_bins=num_bins)
-cdf_data_sparse,bin_edges_sparse = cdf2(data_sparse[:,2],num_bins=num_bins)
-cdf_data_z_sparse,bin_edges_z_sparse = cdf2(data_sparse[:,3],num_bins=num_bins)
+cdf_data,bin_edges = calculate_cdf(data[:,2],num_bins=num_bins)
+cdf_data_sparse,bin_edges_sparse = calculate_cdf(data_sparse[:,2],num_bins=num_bins)
+cdf_data_z_sparse,bin_edges_z_sparse = calculate_cdf(data_sparse[:,3],num_bins=num_bins)
 
 
+plt.close(2)
+plt.figure(2)
+
+sp = cvmodel( data, model=spherical, hs=np.arange(0,50,5), bw=5 )
+plt.plot( sv[0], sv[1], '.-' )
+plt.plot( sv[0], sp( sv[0] ) ) ;
+plt.title('Spherical Model')
+plt.ylabel('Semivariance')
+plt.xlabel('Lag [m]')
+#savefig('semivariogram_model.png',fmt='png',dpi=200)
 
 #%%
 
 # Figure of testdata
 plt.close(1)
 plt.figure(1)
-plt.subplot(2,3,1)
+plt.subplot(3,3,1)
 plt.scatter(data[:,0],data[:,1],c=data[:,2],marker='.')
 plt.colorbar()
 plt.xlim([-1,20])
@@ -173,7 +227,7 @@ plt.title('Full')
 
 x_cdf= bin_edges[1:]-(bin_edges[1]-bin_edges[0])/2
  
-plt.subplot(2,3,4)
+plt.subplot(3,3,4)
 plt.plot(x_cdf,cdf_data,label='cdf_normalized')
 plt.plot(x_cdf,1-cdf_data,label='cdfinv_normalized')
 plt.grid()
@@ -181,7 +235,19 @@ plt.title('cdf')
 plt.legend()
 
 
-plt.subplot(2,3,2)
+plt.subplot(3,3,7)
+# bandwidth, plus or minus 250 meters
+bw = 5
+# lags in 500 meter increments from zero to 10,000
+hs = np.arange(0,50,bw)
+sv = SV( data, hs, bw )
+plt.plot( sv[0], 1-sv[1], '.-' )
+plt.xlabel('Lag [m]')
+plt.ylabel('Semivariance')
+plt.title('Sample Semivariogram') ;
+
+
+plt.subplot(3,3,2)
 plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,2],marker='.')
 plt.xlim([-1,20])
 plt.ylim([-1,25])
@@ -190,14 +256,23 @@ plt.title('Sparse')
 
 x_cdf= bin_edges_sparse[1:]-(bin_edges_sparse[1]-bin_edges_sparse[0])/2
 
-plt.subplot(2,3,5)
+plt.subplot(3,3,5)
 plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
 plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
 plt.grid()
 plt.title('cdf')
 plt.legend()
 
-plt.subplot(2,3,3)
+
+plt.subplot(3,3,8)
+sv_sparse = SV( data_sparse, hs, bw )
+plt.plot( sv_sparse[0], 1-sv_sparse[1], '.-' )
+plt.xlabel('Lag [m]')
+plt.ylabel('Semivariance')
+plt.title('Sample Semivariogram') ;
+
+
+plt.subplot(3,3,3)
 plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,3],marker='.')
 plt.xlim([-1,20])
 plt.ylim([-1,25])
@@ -206,7 +281,7 @@ plt.title('Sparse')
 
 x_cdf= bin_edges_z_sparse[1:]-(bin_edges_z_sparse[1]-bin_edges_z_sparse[0])/2
 
-plt.subplot(2,3,6)
+plt.subplot(3,3,6)
 plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
 plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
 plt.grid()
@@ -216,4 +291,3 @@ plt.xlim([-3,3])
 
 rfuncs.plot_on_secondary_monitor(1)
 
-get
