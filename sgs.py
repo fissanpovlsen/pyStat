77c11a45dd62d0a6b409822a1ef64aa3a009357d
fissanpovlsen <rasmus_bodker@hotmail.com>
2019-08-13 14:49:18 +0200

    Finished sgs.py

--- sgs.py
+++ sgs.py
@@ -43,152 +43,214 @@ def calculate_cdf(d,num_bins=20):
     #cdf[:,1] = 1-cdf[:,0]    
     return cdf,bin_edges
 
-def get_index_for_data_on_grid(data_sparse,data):
+def get_index_for_data_on_grid(data_sparse,x,y):
     k = 0
-    duplicate = np.zeros([50,1])# Duplicate index
+    grid_x = xx.flatten()
+    grid_y = yy.flatten()
+    Nd = np.size(grid_x,0)
+    Nd_sparse = np.size(data_sparse,0)
+    duplicate = np.zeros([Nd_sparse,1])# Duplicate index
     for i in range(Nd):
-        x_test = np.where((data_sparse[:,0]==data[i,0]))
-        y_test = np.where((data_sparse[:,1]==data[i,1]))
+        x_test = np.where((data_sparse[:,0]==grid_x[i]))
+        y_test = np.where((data_sparse[:,1]==grid_y[i]))
         if (len(x_test[0])>0 and len(y_test[0])>0):
             for ii in range(len(x_test[0])):
                 for iii in range(len(y_test[0])):
                     if (x_test[0][ii]==y_test[0][iii]).any():
                         duplicate[k] = i
                         k += 1
-                        print(k)
-    non_duplicate = np.delete(np.arange(500),[duplicate]) # Non-duplicate index
+    duplicate = duplicate.astype(int)
+    non_duplicate = np.delete(np.arange(Nd),[duplicate]) # Non-duplicate index
     return non_duplicate,duplicate
-    
-# Create some data for testing
-Nd = 500    
 
-d = np.ones(Nd)
-d[0:100] = np.random.rand(100)*1
-d[100:200] = np.random.rand(100)*2
-d[200:300] = np.random.rand(100)*3
-d[300:400] = np.random.rand(100)*2.5
-d[400:500] = np.random.rand(100)*1.5
+def get_index_to_nearest_neighbours(data_x,data_y,pt,k=10):
+    import scipy as sp
+    import numpy as np
 
-
-#### 1. Define a grid
-x = np.arange(0,20)
-y = np.arange(0,25)
-
-
-# Initialize data structure
-data = np.zeros([Nd,3])
-data_sparse = np.zeros([50,4])
-
-# Fill in data structure
-data[:,0] = np.repeat(x,25,axis=0)
-data[:,1] = np.tile(y,20)
-data[:,2] = np.copy(d)
-
-
-
-# Take out 50 random points for sparse 
-data_sparse[:,0:3] = data[np.random.choice(Nd,50, replace=False),:]
-
-# 2. Place the z-transformed initial data into the nearest grid cells
-data_sparse[:,3] = sp.stats.zscore(data_sparse[:,2]) # Calculate z-score
-
+    data_coords = np.array([data_x,data_y]).transpose()
+    hej = sp.spatial.distance.cdist(data_coords,pt,'euclidean')
+    indexlist = np.argsort(hej,axis=0)[0:k] # Sort indexes of list 
+    return indexlist # Take k nearest neigbours
 
 # 3. Visit the rest of the grid cells at random and perform kriging using all of the data available
-# Define random path
-non_duplicate,duplicate = get_index_for_data_on_grid(data_sparse,data) # Getting indexes where there is no data
-
-def sgs(datapoints,non_duplicate,Nsim=1):
+def sgs(data_x,data_y,data_val,xx,yy,seed=1,k=10):
+    
+    kriging_points = np.array([data_x,data_y,data_val]).transpose()
+    
+    non_duplicate,duplicate = get_index_for_data_on_grid(kriging_points[:,0:2],x,y) # Getting indexes where there is no data
+    
+    grid_x = xx.flatten()
+    grid_y = yy.flatten()
+       
+    # Define random path
+    np.random.seed(seed) # Seeding
     randpath = np.random.permutation(non_duplicate) # randomize this array
-
-    sim = np.zeros([np.size(randpath),3,Nsim])
-
-    kriging_points = data_sparse[:,0:3]
-
-for ii in range(np.size(randpath)):
-    OK = OrdinaryKriging(kriging_points[:, 0], kriging_points[:, 1], kriging_points[:, 2], variogram_model='gaussian',
+    
+    # Preallocate memory
+    sim = np.zeros([np.size(randpath),3])
+    
+    
+    for ii in range(np.size(randpath)):
+       # print(randpath[ii])
+        sim[ii,0:2] = [grid_x[randpath[ii]], grid_y[randpath[ii]]] # Current point
+        
+        ind_neigh = get_index_to_nearest_neighbours(kriging_points[:, 0],kriging_points[:, 1],[(sim[ii,0],sim[ii,1])],k=k) # Get neighbours for current point
+        #print(ind_neigh)
+        OK = OrdinaryKriging(kriging_points[ind_neigh, 0], kriging_points[ind_neigh, 1], kriging_points[ind_neigh, 2], variogram_model='gaussian',
                      verbose=False, enable_plotting=False) # Ordinary kriging 
     
-    sim[ii,0:2] = data[randpath[ii],0], data[randpath[ii],1]
-    sim[ii,2], ss = OK.execute('grid', sim[ii,0],sim[ii,1]) # Get value for point
+        
+        sim[ii,2], ss = OK.execute('grid', sim[ii,0],sim[ii,1]) # Get value for point
     
-    kriging_points = np.concatenate((data_sparse[:,0:3],sim),axis=0)
+        #kriging_points = np.concatenate((data_sparse[:,0:3],sim),axis=0)
+        kriging_points = np.append(kriging_points,sim[ii,0:3]).reshape((-1,3))
+        #print(kriging_points)
+    return sim,kriging_points
 
+def example(k=10,Nsim = 4):
+    # Create some data for testing
+    Nd = 2000    
+    Nd_sparse = int(Nd/10)
+    
+    d = np.ones(Nd)
+    d[0:400] = np.random.rand(400)+1
+    d[400:800] = np.random.rand(400)+2
+    d[800:1200] = np.random.rand(400)+3
+    d[1200:1600] = np.random.rand(400)+2
+    d[1600:2000] = np.random.rand(400)+1
+    
+    
+    
+    #### 1. Define a grid
+    Nx = 50
+    Ny = 40
+    
+    x = np.arange(0,Nx)
+    y = np.arange(0,Ny)
+    
+    xx,yy = np.meshgrid(x,y)
+    
+    
+    # Initialize data structure
+    data = np.zeros([Nd,3])
+    data_sparse = np.zeros([Nd_sparse,4])
+    
+    # Fill in data structure
+    data[:,0] = xx.flatten()
+    data[:,1] = yy.flatten()
+    data[:,2] = np.copy(d)
+    
+    
+    # Take out 100 random points for sparse 
+    np.random.seed(2)
+    data_sparse[:,0:3] = data[np.random.choice(Nd,Nd_sparse, replace=False),:]
+    
+    
+    # 2. Place the z-transformed initial data into the nearest grid cells
+    data_sparse[:,3] = sp.stats.zscore(data_sparse[:,2]) # Calculate z-score
+    
+    data_x = data_sparse[:,0]
+    data_y = data_sparse[:,1]
+    data_val = data_sparse[:,2]
+    
 
-# Calculate cdfs
-num_bins = 10
-cdf_data,bin_edges = calculate_cdf(data[:,2],num_bins=num_bins)
-cdf_data_sparse,bin_edges_sparse = calculate_cdf(data_sparse[:,2],num_bins=num_bins)
-cdf_data_z_sparse,bin_edges_z_sparse = calculate_cdf(data_sparse[:,3],num_bins=num_bins)
+    sim = [[]]*Nsim
+    # k: Number of nearest neighbours to be included in kriging
+    for isim in range(Nsim):
+        print('Running sim' + str(isim) + '.......', end="\r")
+        rfuncs.tic()
+        sim[isim],kriging_points = sgs(data_x,data_y,data_val,xx,yy,seed=isim+1,k=k)
+        print('DONE!')
+        rfuncs.toc()
+    
+    
+    # Calculate cdfs
+    num_bins = 10
+    cdf_data,bin_edges = calculate_cdf(data[:,2],num_bins=num_bins)
+    cdf_data_sparse,bin_edges_sparse = calculate_cdf(data_sparse[:,2],num_bins=num_bins)
+    cdf_data_z_sparse,bin_edges_z_sparse = calculate_cdf(data_sparse[:,3],num_bins=num_bins)
+    
+    
+    
+    def plotsim(sim,clim):
+        plt.scatter(sim[:,0],sim[:,1],c=sim[:,2],marker='.',s=100)
+        plt.clim(clim)
+        plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,2],marker='.',s=100,edgecolor='k')
+        plt.clim(clim)
+        plt.colorbar()
+        plt.xlim([-1,Nx])
+        plt.ylim([-1,Ny])
+        
+    def plotdata(data,clim):
+        plt.scatter(data[:,0],data[:,1],c=data[:,2],marker='.',s=100)
+        plt.clim(clim)
+        plt.colorbar()
+        plt.xlim([-1,Nx])
+        plt.ylim([-1,Ny])
+    
+    clim = [1,5]
+    
+    # Figure of testdata
+    plt.close(1)
+    plt.figure(1)
+    plt.subplot(2,3,1)
+    plotdata(data,clim)
+    plt.title('Full')
+    
+    x_cdf= bin_edges[1:]-(bin_edges[1]-bin_edges[0])/2
+     
+    plt.subplot(2,3,4)
+    plt.plot(x_cdf,cdf_data,label='cdf_normalized')
+    plt.plot(x_cdf,1-cdf_data,label='cdfinv_normalized')
+    plt.grid()
+    plt.title('cdf')
+    plt.legend()
+    
+    
+    plt.subplot(2,3,2)
+    plotdata(data_sparse,clim)
+    plt.title('Sparse')
+    
+    x_cdf= bin_edges_sparse[1:]-(bin_edges_sparse[1]-bin_edges_sparse[0])/2
+    
+    plt.subplot(2,3,5)
+    plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
+    plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
+    plt.grid()
+    plt.title('cdf')
+    plt.legend()
+    
+    
+    plt.subplot(2,3,3)
+    plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,3],marker='.',s=100)
+    plt.clim([1,5])
+    plt.xlim([-1,Nx])
+    plt.ylim([-1,Ny])
+    plt.colorbar()
+    plt.clim([1,4])
+    plt.title('Sparse')
+    
+    x_cdf= bin_edges_z_sparse[1:]-(bin_edges_z_sparse[1]-bin_edges_z_sparse[0])/2
+    
+    plt.subplot(2,3,6)
+    plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
+    plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
+    plt.grid()
+    plt.title('cdf')
+    plt.legend()
+    plt.xlim([-3,3])
+    
+    
+    rfuncs.plot_on_secondary_monitor(1)
+    
+    
+    
+    plt.close(2)
+    plt.figure(2)
+    for i in range(Nsim):
+        plt.subplot(2,Nsim/2,i+1)
+        plotsim(sim[i],clim)
+        plt.title('SIM '+ str(i))
+    rfuncs.plot_on_secondary_monitor(1)
 
-
-
-#savefig('semivariogram_model.png',fmt='png',dpi=200)
-
-#%%
-
-# Figure of testdata
-plt.close(1)
-plt.figure(1)
-plt.subplot(3,3,1)
-plt.scatter(data[:,0],data[:,1],c=data[:,2],marker='.',s=100)
-plt.colorbar()
-plt.xlim([-1,20])
-plt.ylim([-1,25])
-plt.title('Full')
-
-x_cdf= bin_edges[1:]-(bin_edges[1]-bin_edges[0])/2
- 
-plt.subplot(3,3,4)
-plt.plot(x_cdf,cdf_data,label='cdf_normalized')
-plt.plot(x_cdf,1-cdf_data,label='cdfinv_normalized')
-plt.grid()
-plt.title('cdf')
-plt.legend()
-
-
-plt.subplot(3,3,7)
-plt.scatter(sim[:,0],sim[:,1],c=sim[:,2],marker='.',s=100)
-plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,2],marker='.',s=200)
-plt.colorbar()
-plt.xlim([-1,20])
-plt.ylim([-1,25])
-plt.title('Full')
-
-
-plt.subplot(3,3,2)
-plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,2],marker='.',s=100)
-plt.xlim([-1,20])
-plt.ylim([-1,25])
-plt.colorbar()
-plt.title('Sparse')
-
-x_cdf= bin_edges_sparse[1:]-(bin_edges_sparse[1]-bin_edges_sparse[0])/2
-
-plt.subplot(3,3,5)
-plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
-plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
-plt.grid()
-plt.title('cdf')
-plt.legend()
-
-
-
-plt.subplot(3,3,3)
-plt.scatter(data_sparse[:,0],data_sparse[:,1],c=data_sparse[:,3],marker='.',s=100)
-plt.xlim([-1,20])
-plt.ylim([-1,25])
-plt.colorbar()
-plt.title('Sparse')
-
-x_cdf= bin_edges_z_sparse[1:]-(bin_edges_z_sparse[1]-bin_edges_z_sparse[0])/2
-
-plt.subplot(3,3,6)
-plt.plot(x_cdf,cdf_data_sparse,label='cdf_normalized')
-plt.plot(x_cdf,1-cdf_data_sparse,label='cdfinv_normalized')
-plt.grid()
-plt.title('cdf')
-plt.legend()
-plt.xlim([-3,3])
-
-rfuncs.plot_on_secondary_monitor(1)
-
+        
