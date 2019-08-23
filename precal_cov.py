8480993fef36ee319e518692d6e7343c6bfc9d6d
fissanpovlsen <rasmus_bodker@hotmail.com>
2019-08-23 15:29:16 +0200

    Cleaned code

--- precal_cov.py
+++ precal_cov.py
@@ -11,7 +11,7 @@ Created on Sun Apr 30 22:56:34 2017
 @author: rasmusmadsen
 """
 
-def precal_cov(xx,yy,covtype='gau',sill=1,rang=1):
+def precal_cov(xx,yy,covtype='gau',sill=1,rang=1,nugget=0):
 
     import numpy as np
     
@@ -29,51 +29,30 @@ def precal_cov(xx,yy,covtype='gau',sill=1,rang=1):
     # Preallocate memory for covariance matrix
     cov = np.zeros((num_vars, num_vars), dtype=float)
 
-    
-    #for i, j in np.ndindex(cov.shape):
-        
-    # SLOW ITERATIVE METHOD
-    #tic()
-    #for i, j in np.ndindex(cov.shape):
-            # Slow iterative approach
-  #          dist = np.linalg.norm(pos1[:,i]-pos2[:,j]);
-  #          cov[i, j] = np.exp(-np.dot(dist,dist)/np.dot(1,1)) 
-    #toc()    
-    if covtype == 'gau':
-        # FAST VECTORIZED METHOD
-        for i in range(0, num_vars):
-            # Fast vectorized approach
-            jj=np.arange(0,num_vars)            
-            # positions variable 1
-            p1 = np.tile(pos1[:,i], (num_vars, 1)) #Tile: create num_vars by 1 copies of pos1[:,i]
-                # positions variable 2
-            p2=np.transpose(pos2[:,jj])
-            #cov2[i, :] = np.exp(-np.linalg.norm(p1-p2)**2/1**2)
-            distances = (p1-p2)**2
-            distances = distances.sum(axis=-1)
-            distances = np.sqrt(distances)
-            #\gamma_z(h)=c_0[1-\exp(-\frac{h^2}{a_0^2})]
-    
-            cov[i, :] = sill*(np.exp(-3*distances**2/rang**2))
-    elif covtype == 'exp':
-        # FAST VECTORIZED METHOD
-        for i in range(0, num_vars):
-            # Fast vectorized approach
-            jj=np.arange(0,num_vars)            
-            # positions variable 1
-            p1 = np.tile(pos1[:,i], (num_vars, 1)) #Tile: create num_vars by 1 copies of pos1[:,i]
-                # positions variable 2
-            p2=np.transpose(pos2[:,jj])
-            #cov2[i, :] = np.exp(-np.linalg.norm(p1-p2)**2/1**2)
-            distances = (p1-p2)**2
-            distances = distances.sum(axis=-1)
-            distances = np.sqrt(distances)
-            #\gamma_z(h)=c_0[1-\exp(-\frac{h^2}{a_0^2})]
-            cov[i, :] = sill*(np.exp(-3*distances/rang))
-    elif covtype == 'sph':
-        print('!!!! Spherical covariance type not yet implemented: Try gau or exp !!!!')
-    else:
+    # Check if function arguments are correct
+    if (covtype != 'gau' and covtype != 'exp' and covtype != 'sph'):
         print('!!!! Unknown covariance type: Try gau, exp or sph !!!!')
-        print('!!!! Example: precal_cov(xx,yy,\'gau\',1,2) which is a !!!!')
-        print('!!!! Gaussian covariance type with sill=1 and range=2 !!!!') 
+        print('!!!! Example: precal_cov(xx,yy,covtype=\'gau\',sill=1,rang=2,nugget=0) which is a !!!!')
+        print('!!!! Gaussian covariance type with sill=1, range=2 and nugget = 0 !!!!') 
+        return
+
+    for i in range(0, num_vars):
+        # Fast vectorized approach
+        jj=np.arange(0,num_vars)            
+        # positions variable 1
+        p1 = np.tile(pos1[:,i], (num_vars, 1)) #Tile: create num_vars by 1 copies of pos1[:,i]
+        # positions variable 2
+        p2=np.transpose(pos2[:,jj])
+        #cov2[i, :] = np.exp(-np.linalg.norm(p1-p2)**2/1**2)
+        distances = (p1-p2)**2
+        distances = distances.sum(axis=-1)
+        distances = np.sqrt(distances)
+        if covtype == 'gau':
+            #\gamma_z(h)=c_0[1-\exp(-\frac{h^2}{a_0^2})]    
+            cov[i, :] = nugget+sill*(np.exp(-distances**2/rang**2))   
+        elif covtype == 'exp':
+            #\gamma_z(h)=c_0[1-\exp(-\frac{h^2}{a_0^2})]
+            cov[i, :] = nugget+sill*(np.exp(-distances/rang))
+        elif covtype == 'sph':        
+            print('!!!! Spherical covariance type not yet implemented: Try gau or exp !!!!')
     return cov
